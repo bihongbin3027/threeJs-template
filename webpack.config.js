26ff2e66d1dd82ac9a1e4a6a6624f8ddcaea8519
BiHongBin <410362365@qq.com>
2021-08-04 21:52:42 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/',
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin()
+  ]
+}
