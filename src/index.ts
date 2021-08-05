4dd17cbd9b136b2c32f64a9cf00acd821782a0dc
毕宏斌 <2850208204@qq.com>
2021-08-05 18:07:48 +0800

    feat: 创建第一个动画

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import * as THREE from 'three';
 import '@/style/global.css';
 
+// 场景
 const scene = new THREE.Scene();
+// 透视摄像机，参数：视野角度、长宽比、近截面、远截面
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
@@ -9,8 +11,27 @@ const camera = new THREE.PerspectiveCamera(
   100,
 );
 
+// 渲染器
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-console.log(scene);
+// 创建立方体
+const geometry = new THREE.BoxGeometry();
+// 材质
+const material = new THREE.MeshMatcapMaterial({ color: 0x00ff00 });
+// 网格
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+// 渲染循环
+function animate() {
+  requestAnimationFrame(animate);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
+}
+
+animate();
