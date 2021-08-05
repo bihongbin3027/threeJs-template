d1c2504584046441a9478225c941bb7a51869dde
毕宏斌 <2850208204@qq.com>
2021-08-05 11:51:40 +0800

    feat: 配置css-loader

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import * as THREE from 'three';
-
-export interface ABCDE {
-  name: string;
-}
+import './style/global.css';
 
 const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  100,
+);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
 console.log(scene);
-
-export default scene;
