64196dace55b9d498437b5056bfc6cd1fb6f7740
毕宏斌 <2850208204@qq.com>
2021-08-06 17:45:28 +0800

    update template

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,78 @@
 import * as THREE from 'three';
 import '@/style/global.css';
 
-// 场景
-const scene = new THREE.Scene();
-// 透视摄像机，参数：视野角度、长宽比、近截面、远截面
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  100,
-);
-
 // 渲染器
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(300, 150);
 document.body.appendChild(renderer.domElement);
 
-// 创建立方体
-const geometry = new THREE.BoxGeometry();
+const fov = 75; // 视野范围 垂直方向75度
+const aspect = 2; // 画布宽高比 默认300x150 所以宽高比为300/150=2
+const near = 0.1,
+  far = 5; // 近平面、远平面 限制了摄像机面朝方向的可绘区域，任何距离小于或超过这个范围的物体都将被裁剪掉(不绘制)
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far); // 透视摄像机
+camera.position.z = 2; // 往后移一下摄像机才能显示物体
+
+// 场景
+const scene = new THREE.Scene();
+
+// 立方几何体
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+
 // 材质
-const material = new THREE.MeshMatcapMaterial({ color: 0x00ff00 });
+const material = new THREE.MeshPhongMaterial({ color: 0x44aa88 });
+
 // 网格
 const cube = new THREE.Mesh(geometry, material);
+
+// 将网格添加到场景中
 scene.add(cube);
 
-camera.position.z = 5;
+function render(time?: number) {
+  time *= 0.001;
 
-// 渲染循环
-function animate() {
-  requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  cube.rotation.x = time;
+  cube.rotation.y = time;
+
+  // 场景和摄像机传递给渲染器
   renderer.render(scene, camera);
+
+  requestAnimationFrame(render);
 }
 
-animate();
+render();
+
+// // 渲染器
+// const renderer = new THREE.WebGLRenderer();
+// renderer.setSize(window.innerWidth, window.innerHeight);
+// document.body.appendChild(renderer.domElement);
+
+// // 场景
+// const scene = new THREE.Scene();
+
+// // 透视摄像机，参数：视野角度、长宽比、近截面、远截面
+// const camera = new THREE.PerspectiveCamera(
+//   75,
+//   window.innerWidth / window.innerHeight,
+//   0.1,
+//   100,
+// );
+// camera.position.z = 10;
+
+// // 创建立方体
+// const geometry = new THREE.BoxGeometry();
+// // 材质
+// const material = new THREE.MeshMatcapMaterial({ color: 0x00ff00 });
+// // 网格
+// const cube = new THREE.Mesh(geometry, material);
+// scene.add(cube);
+
+// // 渲染循环
+// function animate() {
+//   requestAnimationFrame(animate);
+//   cube.rotation.x += 0.01;
+//   cube.rotation.y += 0.01;
+//   renderer.render(scene, camera);
+// }
+
+// animate();
