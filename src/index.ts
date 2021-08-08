df44396561ec7a0ed441899cdb55a987fcb1e811
BiHongBin <410362365@qq.com>
2021-08-08 22:10:25 +0800

    refactor: 实例修改

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import '@/style/global.css';
 
 // 渲染器
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(300, 150);
+renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const fov = 75; // 视野范围 垂直方向75度
@@ -11,7 +11,7 @@ const aspect = 2; // 画布宽高比 默认300x150 所以宽高比为300/150=2
 const near = 0.1,
   far = 5; // 近平面、远平面 限制了摄像机面朝方向的可绘区域，任何距离小于或超过这个范围的物体都将被裁剪掉(不绘制)
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far); // 透视摄像机
-camera.position.z = 2; // 往后移一下摄像机才能显示物体
+camera.position.z = 4; // 往后移一下摄像机才能显示物体
 
 // 场景
 const scene = new THREE.Scene();
@@ -19,20 +19,46 @@ const scene = new THREE.Scene();
 // 立方几何体
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 
-// 材质
-const material = new THREE.MeshPhongMaterial({ color: 0x44aa88 });
+// 添加灯光效果
+{
+  const color = 0xFFFFFF
+  const intensity = 1
+  const light = new THREE.DirectionalLight(color, intensity)
+  light.position.set(-1, 2, 4)
+  scene.add(light)
+}
 
-// 网格
-const cube = new THREE.Mesh(geometry, material);
+function makeInstance(geometry: THREE.BoxGeometry, color: number, x: number) {
+  // 材质
+  const material = new THREE.MeshPhongMaterial({ color })
 
-// 将网格添加到场景中
-scene.add(cube);
+  // 网格
+  const cube = new THREE.Mesh(geometry, material)
+
+  // 网格加入到场景
+  scene.add(cube)
+
+  // 设置立方体位置
+  cube.position.x = x
+
+  return cube
+}
+
+const cubes = [
+  makeInstance(geometry, 0x8844aa, -2),
+  makeInstance(geometry, 0x44aa88,  0),
+  makeInstance(geometry, 0xaa8844,  2),
+];
 
 function render(time?: number) {
   time *= 0.001;
 
-  cube.rotation.x = time;
-  cube.rotation.y = time;
+  cubes.forEach((cube, ndx) => {
+    const speed = 1 + ndx * .1
+    const rot = time * speed
+    cube.rotation.x = rot
+    cube.rotation.y = rot
+  })
 
   // 场景和摄像机传递给渲染器
   renderer.render(scene, camera);
@@ -41,38 +67,3 @@ function render(time?: number) {
 }
 
 render();
-
-// // 渲染器
-// const renderer = new THREE.WebGLRenderer();
-// renderer.setSize(window.innerWidth, window.innerHeight);
-// document.body.appendChild(renderer.domElement);
-
-// // 场景
-// const scene = new THREE.Scene();
-
-// // 透视摄像机，参数：视野角度、长宽比、近截面、远截面
-// const camera = new THREE.PerspectiveCamera(
-//   75,
-//   window.innerWidth / window.innerHeight,
-//   0.1,
-//   100,
-// );
-// camera.position.z = 10;
-
-// // 创建立方体
-// const geometry = new THREE.BoxGeometry();
-// // 材质
-// const material = new THREE.MeshMatcapMaterial({ color: 0x00ff00 });
-// // 网格
-// const cube = new THREE.Mesh(geometry, material);
-// scene.add(cube);
-
-// // 渲染循环
-// function animate() {
-//   requestAnimationFrame(animate);
-//   cube.rotation.x += 0.01;
-//   cube.rotation.y += 0.01;
-//   renderer.render(scene, camera);
-// }
-
-// animate();
